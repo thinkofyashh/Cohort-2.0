1c0b1a22135dc7e1bf92dad473ab721bee84efc6
yashrawat <yashrawat682@gmail.com>
2024-02-17 18:34:54 +0530

    Need for Global Catches

---
+++ week3/inputValidationNeed.js
@@ -0,0 +1,10 @@
+const express=require("express")
+const app=express()
+app.use(express.json())
+app.post("/",function(req,res){
+    const kidneys=req.body.kidneys
+    const kidneyLength=kidneys.length;
+    res.send("your have "+ kidneyLength +  " kidneys.")
+
+})
+app.listen(3000)
