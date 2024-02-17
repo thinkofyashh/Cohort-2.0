52a72d76eacf5fb406627119cc9fee06716ef624
yashrawat <yashrawat682@gmail.com>
2024-02-17 18:20:25 +0530

    Using middleWares

--- week3/middlewaresHospital.js
+++ week3/middlewaresHospital.js
@@ -1,20 +1,33 @@
 const express=require("express")
+const app=express();
 
-const app=express()
 
-app.get("/",function(req,res){
-    const username=req.headers.username
-    const pass=req.headers.pass
+function userMiddlewares(req,res,next){
+    const username=req.headers.username;
+    const pass=req.headers.pass;
+    if(username!="yash" || pass != 1234){
+        res.json({
+            msg:"wrong credentials"
+        })
+    }else{
+        next()
+    }
+
+}
+function kidneyMiddlewares(req,res,next){
     const kidneyId=req.query.kidneyId
-    if(!(username==="yash" && pass==="1234")){
-        res.status(403).json({msg:"you are not authenticated"})
-        return ;
-    }
     if(kidneyId!=1 && kidneyId!=2){
-        res.status(403).json({msg:"your kidney is not fine."})
-        return ;
+        res.json({msg:"wrong inputs"})
+    }else{
+        next()
     }
+}
+app.use(userMiddlewares,kidneyMiddlewares)
+app.get("/",function(req,res){
 
-    res.send("your kidney is fine ")
+    res.json({
+        msg:"your kidney is good ."
+    })
 })
+
 app.listen(3000)
