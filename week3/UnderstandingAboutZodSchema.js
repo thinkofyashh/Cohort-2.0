4f5605c5fca3939724c6985d4aa204ce388712a4
yashrawat <yashrawat682@gmail.com>
2024-02-18 07:04:54 +0530

    validating email and password using Zod Library.

--- week3/UnderstandingAboutZodSchema.js
+++ week3/UnderstandingAboutZodSchema.js
@@ -1,20 +1,26 @@
 const zod=require("zod");
-
-
-// create a scheme and validate it .wheather the user has created a valid schema or not .
-
-// email --> string @gmail.com
-// password --> string atleast 5 character
-//country --> exactly India 
+const express=require("express")
+const app=express()
 function validateInput(obj){
     const schema=zod.object({
         email:zod.string().email(),
         password:zod.string().min(5),
-        country:zod.literal("India")
     })
     const response=schema.safeParse(obj)
-    console.log(response)
+    return response
 }
+app.use(express.json())
+app.post("/",function(req,res){
+    const response=validateInput(req.body)
+    if(!response.success){
+        res.send("Inputs are invalid")
+    }else{
+        res.send("successfully login")
+    }
+
+})
 validateInput({
     email:"yashrawat682@gmail.com",password:"12345",country:"India"
 })
+
+app.listen(3000)
