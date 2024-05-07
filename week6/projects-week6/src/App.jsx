8df555f41196749dd91a2e23d471575ce56d45f3
yashrawat <yashrawat682@gmail.com>
2024-05-07 16:19:17 +0530

    Using usememo hook

--- week6/projects-week6/src/App.jsx
+++ week6/projects-week6/src/App.jsx
@@ -1,32 +1,27 @@
-import { useState } from "react"
+import React, { useState } from "react"
+import {useMemo} from "react";
 function App() {
+ const [count,setCount]=useState("my name is Yash");
+function updateTitle(){
+  setCount("my name is " + Math.random())
+}
   return (
    <div>
-       <HeaderWithButton></HeaderWithButton>
-      <Header title="my name is raman1"></Header>
+      <button onClick={updateTitle}>Click me to Change the Title</button>
+     <Header title={count}></Header>
       <Header title="my name is raman2"></Header>
-      <Header title="my name is raman22"></Header>
-      <Header title="my name is raman31"></Header>
+      <Header title="my name is raman32"></Header>
+      <Header title="my name is ramane"></Header>
+      <Header title="my name is ramanrsrf"></Header>
+      <Header title="my name is ramanrs"></Header>
+
    </div>
   )
 }
-function HeaderWithButton(props){
-  const [title,setTitle]=useState("my name is yash")
-
-  function onClicked(){
-    setTitle("My name is "+ Math.random());
-  }
-  return <>
-  <button onClick={onClicked}>Click the Button</button>
-  <Header title={title}></Header>
-  
-  </>
-
-}
-function Header(props){
+const Header=React.memo(function Header(props){
   return <div>
     {props.title}
   </div>
 
-}
+})
 export default App
