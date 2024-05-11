57bc30cbca3738a974c2ac18ad60d8622d2dbe91
yashrawat <yashrawat682@gmail.com>
2024-05-11 19:09:35 +0530

    learned about Wrapper Components

--- week6/projects-week6/src/App.jsx
+++ week6/projects-week6/src/App.jsx
@@ -1,44 +1,26 @@
-import React, { useState } from "react"
-import {useMemo} from "react";
-let counter =4;
 function App() {
-  const [todos,setTodos]=useState([{
-    id:1,
-    title:"go to gym",
-    description:"from 6-9 pm"
-  },
-  {
-    id:2,
-    title:"DSA",
-    description:"from 9-12 pm"
-  },
-  {
-    id:3,
-    title:"Dev",
-    description:"from 2-5 pm"
-  }])
-  function updateTodo(){
-    setTodos([...todos,{
-      id:counter++,
-      title:Math.random(),
-      description:Math.random()
-    }])
-  }
-  return (
-   <div>
-    <button onClick={updateTodo}>Add Todo</button>
 
-   {todos.map(function(todo){
-    return <Todo title={todo.title} description={todo.description}></Todo>
-   })}
-   </div>
-  )
+ return <div>
+  <CardWrapper>
+    <div>Hi there </div>
+    <CardWrapper><TextComponent></TextComponent></CardWrapper>
+
+  </CardWrapper>
+
+  
+ </div>
 }
-function Todo(props){
+
+function TextComponent(){
   return <div>
-    <div>{props.title}</div>
-    <div>{props.description}</div>
+    hit here 
   </div>
+}
+function CardWrapper(props){
+return <div style={{border:"2px solid black", padding:20, margin:20}}>
+  {props.children}
+  </div>
+  
 }
 
 
