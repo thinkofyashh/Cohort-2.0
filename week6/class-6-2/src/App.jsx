c3a0177e91c6319ef1337e1a8467ed61c7a92093
yashrawat <yashrawat682@gmail.com>
2024-05-16 20:25:07 +0530

    Without useMemo hook

--- week6/class-6-2/src/App.jsx
+++ week6/class-6-2/src/App.jsx
@@ -5,43 +5,27 @@ import { set } from 'zod';
 
 function App() {
 
-  const [count,setCount]=useState(1);
+  let [count,setCount]=useState(1);
+  let [Inputvalue,setInputvalue]=useState(0);
+
+  let counting =0;
+  for(let i=0;i<=Inputvalue;i++){
+    counting=counting+i;
+    
+  }
 
 
   return (
    <>
-   <button onClick={()=>{setCount(1)}}>1</button>
-   <button onClick={()=>{setCount(2)}}>2</button>
-   <button onClick={()=>{setCount(3)}}>3 </button>
-   <button onClick={()=>{setCount(4)}}>4</button>
-   <Todo id ={count}></Todo>
+   <input placeholder='Enter the Number ' onChange={function(e){
+    setInputvalue(e.target.value)
+   }}></input><br></br>
+   Sum of {Inputvalue} is {counting}
+  <br></br>
+   <button onClick={()=>{setCount(count=count+1)}}>Counter ({count})</button>
+
+  
    </>
   )
 }
-
-function Todo({id}){
-// use state 
-  const [Todos,setTodos]=useState({});
-// use effect
-  useEffect(()=>{
-    async function fetchData(){
-    try{
-        const res =await axios.get("https://sum-server.100xdevs.com/todo?id="+ id)
-        setTodos(res.data.todo)
-
-    }catch(err){
-      console.log("error")
-    }
-    }
-    // aysnc-await function 
-    fetchData();
-  },[id])
-
-
-  return <div>
-    <h1> {Todos.title}  </h1>
-    {Todos.description}
-  </div>
-}
-
 export default App
