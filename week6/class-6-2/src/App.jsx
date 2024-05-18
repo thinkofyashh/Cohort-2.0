7735c21934661df5dd7173c5cfe4dc96d80cf1b5
yashrawat <yashrawat682@gmail.com>
2024-05-18 20:44:40 +0530

    learned about referntial equality and a program without useCallback hooks

--- week6/class-6-2/src/App.jsx
+++ week6/class-6-2/src/App.jsx
@@ -1,39 +1,27 @@
 import { useEffect, useMemo, useState } from 'react'
+import { memo } from 'react';
+
 import axios from "axios"; 
 import './App.css'
 import { set } from 'zod';
-
 function App() {
-
-  let [count,setCount]=useState(1);
-  let [Inputvalue,setInputvalue]=useState(0);
-
-
-
-  
-  
-  const counted =useMemo(()=>{
-    console.log("memo is being called")
-    let counting =0;
-  for(let i=0;i<=Inputvalue;i++){
-    counting=counting+i;
-    
-  }
-  return counting;
-  },[Inputvalue])
-
-
+let [count,setCount]=useState(1);
+// changed to function 
+function a(){
+  console.log("hi there")
+}
   return (
    <>
-   <input placeholder='Enter the Number ' onChange={function(e){
-    setInputvalue(e.target.value)
-   }}></input><br></br>
-   Sum of {Inputvalue} is {counted}
-  <br></br>
    <button onClick={()=>{setCount(count=count+1)}}>Counter ({count})</button>
-
-  
+   <Demo a={a}></Demo>  
    </>
   )
 }
+// memo will only run this function when somthing is changed in a . 
+const Demo=memo(function({a}){
+  console.log("re rendered demo");  // we put a log to check this 
+  return <div>
+    hello there {a}
+  </div>
+})
 export default App
