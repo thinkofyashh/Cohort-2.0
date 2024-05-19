394549c852ab6374d19153b930e8ca29cf5086f2
yashrawat <yashrawat682@gmail.com>
2024-05-19 20:22:37 +0530

    Manually manipulating DOM

--- week6/class6.3/src/App.jsx
+++ week6/class6.3/src/App.jsx
@@ -2,39 +2,19 @@ import { memo, useCallback, useEffect, useMemo, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [exchange1Data,setExchange1Data] = useState({});
-  const [exchange2Data,setExchange2Data] = useState({});
-  const [bankdata,setBankData]=useState({})
-  useEffect(function(){
-    setTimeout(()=>{
-      setBankData({income:100})
-    },3000)
-  },[])
-  useEffect(function(){
-    setTimeout(()=>{
-      setExchange1Data({returns:100})
-    },1000);
-  },[])
-  useEffect(function(){
-    setTimeout(()=>{
-      setExchange2Data({returns:100})
-    },1000);
-  },[])
 
-  const calculateReturns=useCallback(function(){
-    return exchange1Data.returns+exchange2Data.returns;
-  },[exchange1Data,exchange2Data])
-  const incomeTax=(bankdata.income+calculateReturns())*0.3
+  useEffect(()=>{setTimeout(()=>{
+
+    document.getElementById("incomeTaxContainer").innerHTML=10
+
+
+  },5000)},[])
+  const incomeTax=2000
   return (
     <>
-    <CrpytoReturns calculateReturns={calculateReturns}></CrpytoReturns>
+    hi your income tax is <div id="incomeTaxContainer">{incomeTax}</div>
     </>
   )
 }
-const CrpytoReturns=memo(function({calculateReturns}){
-  console.log("Crypto Re- rendered  ")
-  return <div>
-    Hi there your Cryto Returns are {calculateReturns()}
-  </div>
-})
+
 export default App
