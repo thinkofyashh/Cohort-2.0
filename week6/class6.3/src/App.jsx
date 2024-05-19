9e4a022e5295914e4f3caaf98f9574183d5fd9e1
yashrawat <yashrawat682@gmail.com>
2024-05-19 20:05:50 +0530

    Learned about memo hook and useCallback hook

--- week6/class6.3/src/App.jsx
+++ week6/class6.3/src/App.jsx
@@ -1,40 +1,40 @@
-import { useEffect, useMemo, useState } from 'react'
+import { memo, useCallback, useEffect, useMemo, useState } from 'react'
 import './App.css'
 
 function App() {
   const [exchange1Data,setExchange1Data] = useState({});
   const [exchange2Data,setExchange2Data] = useState({});
   const [bankdata,setBankData]=useState({})
-
-  console.log("re - rendered")
-
   useEffect(function(){
     setTimeout(()=>{
       setBankData({income:100})
     },3000)
   },[])
-
   useEffect(function(){
     setTimeout(()=>{
       setExchange1Data({returns:100})
     },1000);
   },[])
-
   useEffect(function(){
     setTimeout(()=>{
       setExchange2Data({returns:100})
     },1000);
   },[])
 
-
-  const exhangereturns = useMemo(()=>{
+  const calculateReturns=useCallback(function(){
     return exchange1Data.returns+exchange2Data.returns;
   },[exchange1Data,exchange2Data])
-  const incomeTax=(bankdata.income+exhangereturns)*0.3
+  const incomeTax=(bankdata.income+calculateReturns())*0.3
   return (
     <>
-    hi there , your income tax returns are {incomeTax}
+    <CrpytoReturns calculateReturns={calculateReturns}></CrpytoReturns>
     </>
   )
 }
+const CrpytoReturns=memo(function({calculateReturns}){
+  console.log("Crypto Re- rendered  ")
+  return <div>
+    Hi there your Cryto Returns are {calculateReturns()}
+  </div>
+})
 export default App
