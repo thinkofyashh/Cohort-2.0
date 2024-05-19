03fe0b5f128f71f8ae25b08d71160ba7585e9bc0
yashrawat <yashrawat682@gmail.com>
2024-05-19 19:10:49 +0530

    learned about useMemo using Real life Example

--- week6/class6.3/src/App.jsx
+++ week6/class6.3/src/App.jsx
@@ -1,8 +1,9 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [exchangeData,setExchangeData] = useState({});
+  const [exchange1Data,setExchange1Data] = useState({});
+  const [exchange2Data,setExchange2Data] = useState({});
   const [bankdata,setBankData]=useState({})
 
   console.log("re - rendered")
@@ -15,13 +16,21 @@ function App() {
 
   useEffect(function(){
     setTimeout(()=>{
-      setExchangeData({returns:100})
+      setExchange1Data({returns:100})
     },1000);
   },[])
 
-  
+  useEffect(function(){
+    setTimeout(()=>{
+      setExchange2Data({returns:100})
+    },1000);
+  },[])
 
-  const incomeTax=(bankdata.income+exchangeData.returns)*0.3
+
+  const exhangereturns = useMemo(()=>{
+    return exchange1Data.returns+exchange2Data.returns;
+  },[exchange1Data,exchange2Data])
+  const incomeTax=(bankdata.income+exhangereturns)*0.3
   return (
     <>
     hi there , your income tax returns are {incomeTax}
