a5dab893e028377e0de4510d5bbc123233c306dd
yashrawat <yashrawat682@gmail.com>
2024-05-27 19:11:58 +0530

    Done with Assignment

--- week5/Assignment/frontend_BusinessCard/src/App.jsx
+++ week5/Assignment/frontend_BusinessCard/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <Card name="Yash" description="A Student in 100xDevs"> </Card> 
+      <Card name="Yash" description="A Student in 100xDevs" interests={["ioninc","web dev","app dev"]} linkedIn="https://www.youtube.com/" twitter="https://github.com/thinkofyashh"> </Card> 
      
     </>
   )
--- week5/Assignment/frontend_BusinessCard/src/components/BusinessCard.jsx
+++ week5/Assignment/frontend_BusinessCard/src/components/BusinessCard.jsx
@@ -1,34 +1,61 @@
 export function Card(props){
-    const divStyle = {
-        width: '400px',
+    const Style = {
+        divStyle:{
+         width: '400px',
         height: '400px',
         backgroundColor: '#f0f0f0',
         boxShadow: '1px 1px 5px #888888',
         borderRadius:'10px'
+        },
+        headOne:{
+            paddingLeft:20, 
+            paddingTop:20, 
+            marginBottom:0, 
+            paddingBottom:0
+        },
+        Para:{
+            paddingLeft:20 , 
+            marginTop:0
+        } ,
+        headThree:{
+            paddingLeft:20, 
+            marginTop:0, 
+            marginBottom:0
+        }
+            ,
+        unorderedStyle:{
+            marginTop:0,
+             marginBottom:0
+            } ,
+        interestItem:{
+            marginTop:0, 
+            marginBottom:20
+        },
+        ButtonStyle:{
+        backgroundColor:'#1877F2', 
+        margin:20,
+         padding:20, 
+         borderRadius:'10px', 
+         color:'white'
+        }    
+
+
+
       };
      // {border:'2px solid black', borderRadius:10, boxShadow:10  }
     return (
-        <div style={divStyle}>
-
-        <h1 style={{paddingLeft:20, paddingTop:20, marginBottom:0, paddingBottom:0}}>{props.name}</h1> <br></br>
-
-        <p style={{paddingLeft:20 , marginTop:0}}>{props.description}</p> <br></br>
-
-        <h3 style={{paddingLeft:20, marginTop:0, marginBottom:0}}>Interests</h3> <br></br>
-
-        <ul style={{marginTop:0, marginBottom:0}}>
-            <li style={{margin:5}}>Ionic</li>
-            <li style={{margin:5}}>Open Source</li>
-            <li style={{margin:5}}>App Dev</li>
+        <div style={Style.divStyle}>
+        <h1 style={Style.headOne}>{props.name}</h1> <br></br>
+        <p style={Style.Para}>{props.description}</p> <br></br>
+        <h3 style={Style.headThree}>Interests</h3> <br></br>
+        <ul style={Style.interestItem}>
+            {props.interests.map((interest)=>{
+                return <li >{interest}</li>
+            })}
         </ul> <br></br>
-
-        <button style={{backgroundColor:'#1877F2', margin:20, padding:20, borderRadius:'10px', color:'white'}}>LinkedIn</button>
-        <button style={{backgroundColor:'#1877F2', padding:20, borderRadius:'10px', margin:20, color:'white'}}>Twitter</button>
-
-
-
-    
-        
+        <a href={props.linkedin} style={Style.ButtonStyle}>Linkedin</a>
+        <a href={props.twitter} style={Style.ButtonStyle}>Twitter</a>
         </div>
     )
 }
+
