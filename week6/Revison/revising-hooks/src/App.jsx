782216faa3e3dcd51eea578e4916585e3737efd5
yashrawat <yashrawat682@gmail.com>
2024-05-28 18:35:36 +0530

    Problem unessary re - rendering

--- week6/Revison/revising-hooks/src/App.jsx
+++ week6/Revison/revising-hooks/src/App.jsx
@@ -1,10 +1,33 @@
-function App() {
+import { useState } from "react"
 
+function App() {
+const [title,setTitle]=useState("Hello my name is Yash")
+
+console.log("Re - rendered")
+
+function onClickHandler(){
+  setTitle("Hello my name is "+Math.random())
+}
   return (
     <>
-      hi there 
+    <button onClick={onClickHandler}>Some Thing Will Happen</button>
+    <Header title="Hello my nmae is Raman"></Header>
+    <Header title="Hello my name is mini"></Header>
+    <Header title="hello my name is suchata"></Header>
+    <Header title="Hello my name is RamanBahlala"></Header>
+    <Header title="hello my name is chandu"></Header>
+    <Header title={title}></Header>
     </>
   )
+}
+
+function Header({title}){
+  return (
+    <>
+    <div>{title}</div>
+    </>
+  )
+
 }
 
 export default App
