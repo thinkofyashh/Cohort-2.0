8b2afaad5f1120a16a663217d3255ad0f3be7d42
yashrawat <yashrawat682@gmail.com>
2024-05-29 17:01:19 +0530

    understanding the Flow of code while working with useEffect

--- week6/Revison/revising-hooks/src/App.jsx
+++ week6/Revison/revising-hooks/src/App.jsx
@@ -1,41 +1,71 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
 function App() {
-  console.log("Main rendered")
-  return (
-    <> 
+  const [Todos,setTodo]=useState([{id:1,title:"DSA",description:"From 4 to 5 pm"}])
+  console.log("App re - rendered")
 
-    <Todo id={3}></Todo>
-    <Todo id={5}></Todo>
+  useEffect(()=>{
+    async function fetchData(){
+      console.log("useEffect called")
+      const res=await axios("https://sum-server.100xdevs.com/todos")
+      setTodo(res.data.todos)
+    }
+
+    fetchData()
+  },[])
+
+  return (
+    <>
+
+{Todos.map((todo)=>{
+  return <Todo title={todo.title} description={todo.description} id={todo.id} key={todo.id}></Todo>
+})}
+this is custom 
+<CustomTodo id={3}></CustomTodo>
+<CustomTodo id={4}></CustomTodo>
+<CustomTodo id={5}></CustomTodo>
+
+    
+
+  
+    </>
+  )
+}
+
+function Todo({title,description,id}){
+
+  return (
+    <>
+    <h1>{title}</h1>
+    <h2>{description}</h2>
+    <h3>{id}</h3>
+
+    
     </>
   )
 
 }
 
-function Todo({id}){
-const [Todos,setTodo]=useState({});
+function CustomTodo({id}){
+  console.log("Custom")
+  const [count,setCount]=useState({});
   useEffect(()=>{
-    async function fetchData(){
-      console.log("inside UseEffect")
-
-      const res= await axios.get("https://sum-server.100xdevs.com/todo?id="+id)
-      setTodo(res.data.todo);
-
-
+    async function fetching(){
+      console.log("useEffect of custom gets called")
+      const res=await axios.get("https://sum-server.100xdevs.com/todo?id="+id)
+      setCount(res.data.todo)
 
     }
-    fetchData()
+    fetching()
   },[id])
+  return (
+    <>
+    <h1>{count.title}</h1>
+    <h2>{count.description}</h2>
 
-  return (<>
-  <h1>{Todos.title}</h1>
-  <h2>{Todos.description}</h2>
-  </>)
-
+    </>
+  )
 
 }
-
-
-
 
 export default App
