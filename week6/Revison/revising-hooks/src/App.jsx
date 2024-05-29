a410e1901cd4ea032fb77fe651da3c8bf768f1da
yashrawat <yashrawat682@gmail.com>
2024-05-29 16:36:07 +0530

    revised about useEffect Hook

--- week6/Revison/revising-hooks/src/App.jsx
+++ week6/Revison/revising-hooks/src/App.jsx
@@ -1,46 +1,41 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
 function App() {
-  const [Todos,setTodo]=useState([{id:1,title:"DSA",description:"From 4 to 5 pm"}])
-  console.log("App re - rendered")
+  console.log("Main rendered")
+  return (
+    <> 
 
+    <Todo id={3}></Todo>
+    <Todo id={5}></Todo>
+    </>
+  )
+
+}
+
+function Todo({id}){
+const [Todos,setTodo]=useState({});
   useEffect(()=>{
     async function fetchData(){
-      console.log("useEffect called")
-      const res=await axios("https://sum-server.100xdevs.com/todos")
-      setTodo(res.data.todos)
+      console.log("inside UseEffect")
+
+      const res= await axios.get("https://sum-server.100xdevs.com/todo?id="+id)
+      setTodo(res.data.todo);
+
+
+
     }
-
     fetchData()
-  },[])
+  },[id])
 
-  return (
-    <>
-
-{Todos.map((todo)=>{
-  return <Todo title={todo.title} description={todo.description} id={todo.id} key={todo.id}></Todo>
-})}
+  return (<>
+  <h1>{Todos.title}</h1>
+  <h2>{Todos.description}</h2>
+  </>)
 
 
-    
-
-  
-    </>
-  )
 }
 
-function Todo({title,description,id}){
 
-  return (
-    <>
-    <h1>{title}</h1>
-    <h2>{description}</h2>
-    <h3>{id}</h3>
 
-    
-    </>
-  )
-
-}
 
 export default App
