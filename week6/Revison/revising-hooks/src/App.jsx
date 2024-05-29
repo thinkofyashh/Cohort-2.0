d6d66d3bde2975d420cdd06fdbc02642d4e52528
yashrawat <yashrawat682@gmail.com>
2024-05-29 15:24:08 +0530

    Wrapper components

--- week6/Revison/revising-hooks/src/App.jsx
+++ week6/Revison/revising-hooks/src/App.jsx
@@ -1,48 +1,29 @@
-import { useState } from "react"
-import React,{Fragment} from "react"
+
 function App() {
-
-const [Todos,setTodos]=useState([
-    {
-    id:1,
-    title:"Gym",
-    description:"from 6to 8 pm"
-  },{
-    id:2,
-    title:"DAS",
-    description:"from 6to 8 pm"
-  },{
-    id:3,
-    title:"Ai",
-    description:"from 6to 8 pm"
-  }
-])
-function onClickHandler(){
-  setTodos([...Todos,{
-    id:Math.random(),
-    title:Math.random(),
-    description:Math.random()
-  }])
-
-    
-}
   return (
     <>
-    {Todos.map((todo)=>{
-      return <Todo key={todo.id} id={todo.id} title={todo.title} description={todo.description} ></Todo>
-    })}
-    <button onClick={onClickHandler}>Add Todo</button>
+    <CardWrapper>
+
+<Greet></Greet>
+
+    </CardWrapper>
+
+    <CardWrapper>
+
+      Hello again 
+    </CardWrapper>
     </>
   )
 }
-const Todo=React.memo(function Todo({title,description,id}){
-  console.log("Todo Re rendered")
+function Greet(){
   return <>
-  <h1>{id}</h1>
-  <h1>{title}</h1>
-  <h2>{description}</h2>
-  
-  
+  hello there welcome 
   </>
-})
+}
+function CardWrapper({children}){
+  return (
+    <div style={{border:"20px solid black", padding:20, margin:20}}>{children}</div>
+  )
+}
+
 export default App
