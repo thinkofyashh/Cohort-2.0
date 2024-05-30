0a3559a9018939aac83f1285a6d4f5655990138a
yashrawat <yashrawat682@gmail.com>
2024-05-30 15:55:24 +0530

    Manually Manupulating DOm

--- week6/Revison/revising-hooks/src/App.jsx
+++ week6/Revison/revising-hooks/src/App.jsx
@@ -2,48 +2,26 @@ import axios from "axios"
 import React, { useEffect, useMemo, useState,Fragment, useCallback } from "react"
 function App() {
 
-  const [count,setCount]=useState(0);
+  useEffect(()=>{
+    setTimeout(()=>{
+      document.getElementById("container").innerHTML=10
+      },5000)
+  },[])
 
-function onClickHandler(){
-  setCount(count+1)
-}
-// primitive dataype
-var a=1;
-// non primitive datatype
-var b= useCallback(()=>{
-  function b(){
-    console.log("hello there")
-  }
-},[])
-  return (
+
+  const IncomeTax=3000
+ return (
     <>  
-    <button onClick={onClickHandler}>counter {count}</button> <br></br>
-    <Demo a={a}></Demo><br></br>
-    <Demo1 b={b}></Demo1>
+
+    hi your income tax is <div id="container">{IncomeTax}</div>
+
+    
     
     </>
   )
 }
-// is being getting called again again beacuse of non primitive datatype as react thinking something has chanaegd . beacuse everytime the react re renders it create new instance so it changes refrences
-const Demo1=React.memo(function Demo1(){
-  console.log("memo1 got called")
-  return (
-    <>
-    hi 
-    
-    </>
-  )
-})
-// its a primitive data types react compres its with value .
-const Demo=React.memo(function Demo(){
-  console.log("memo got called")
-  return (
-    <>
-    hi three
-    
-    </>
-  )
-})
+
+
 
 
 export default App
