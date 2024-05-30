155ebfd7a71ea264450e6476a0d278dc7c101395
yashrawat <yashrawat682@gmail.com>
2024-05-30 13:47:53 +0530

    revised useMemo

--- week6/Revison/revising-hooks/src/App.jsx
+++ week6/Revison/revising-hooks/src/App.jsx
@@ -1,22 +1,23 @@
 import axios from "axios"
-import { useEffect, useState } from "react"
+import { useEffect, useMemo, useState } from "react"
 function App() {
 
   const [count,setCount]=useState(0);
   let [inputValue,setInputValue]=useState(0);
-  let [finalValue,setfinalValue]=useState(0)
 
 function onClickHandler(){
   setCount(count+1)
 }
-// the problem with this apprach is as it causes unnessasry re rendering . everytime the input value change(it triggers re render) and everytime finalvalue changes (it again causes re renders).
-useEffect(()=>{
-  console.log("useEffect got called")
+
+
+let counting =useMemo(()=>{
+  console.log("use memo gets called ")
   let counting=0;
   for(let i=0;i<=inputValue;i++){
     counting=counting+i
   }
-  setfinalValue(counting);
+  return counting
+
 },[inputValue])
   return (
     <>  
@@ -25,7 +26,7 @@ useEffect(()=>{
       setInputValue(e.target.value)
     }}></input>
     <br></br>
-    <div>{finalValue}</div>
+    <div>{counting}</div>
     </>
   )
 }
