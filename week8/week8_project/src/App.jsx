0f0cdcee4a83959a22a05482bd704db22de729d6
yashrawat <yashrawat682@gmail.com>
2024-07-03 17:54:07 +0530

    using flex without tailwind

---
+++ week8/week8_project/src/App.jsx
@@ -0,0 +1,15 @@
+import Test from "./components/FlexTest"
+
+function App() {
+  return (
+    <>
+
+    <Test></Test>
+
+   
+     
+    </>
+  )
+}
+
+export default App
---
+++ week8/week8_project/src/components/FlexTest.jsx
@@ -0,0 +1,15 @@
+export default function Test(){
+    return(
+        <div style={{display:"flex", justifyContent:"space-evenly"}}>
+            <div style={{background:"green"}}>
+                hey there from the first div
+            </div>
+            <div style={{background:"Pink"}}>
+                Hi there from the second div
+            </div>
+            <div style={{background:"red"}}>
+                Hi there from the third div
+            </div>
+        </div>
+    )
+}
