44e648f87b519e1776e4c3accbfe1326da0f368d
yashrawat <yashrawat682@gmail.com>
2024-07-03 19:27:10 +0530

    responsiveness using tailwind basics

--- week8/week8_project/src/components/FlexTest.jsx
+++ week8/week8_project/src/components/FlexTest.jsx
@@ -1,13 +1,13 @@
 export default function Test(){
     return(
-        <div style={{display:"flex", justifyContent:"space-evenly"}}>
-            <div style={{background:"green"}}>
+        <div className="grid grid-cols-12">
+            <div className="col-span-12 md: col-span-5 ">
                 hey there from the first div
             </div>
-            <div style={{background:"Pink"}}>
+            <div className="col-span-12 md: col-span-5">
                 Hi there from the second div
             </div>
-            <div style={{background:"red"}}>
+            <div className="col-span-12 md: col-span-2">
                 Hi there from the third div
             </div>
         </div>
