edc53ec48f5ea48f896300d3ab04956ce9e04b30
yashrawat <yashrawat682@gmail.com>
2025-01-13 17:01:28 +0530

    Starting with the Interfaces in typescript

--- week9/node-app/src/a.ts
+++ week9/node-app/src/a.ts
@@ -1,43 +1,27 @@
+import { number, object } from "zod";
 
-// declaring varible in the typescript 
-let lastname="hello";
-
-// creating function in the typescript 
-
-function greet (firstname){
-    return console.log("hello "+firstname);
+const user:User={
+    firstname:"yash",
+    lastname:"rawat",
+    email:"yashrawat682@gmail.com",
+    age:22
+};
+interface User{
+    firstname:string,
+    lastname:string,
+    email?:string,
+    age:number
+};
+function isLegal(user:User):Boolean{
+    if(user.age > 18) return true;
+    else return false;
+}
+function greet(user:User):void{
+console.log(user.firstname);
 }
 
-//creating a function to find out sum of two number.
-function sum(a:number , b:number):number{
-    return a+b;
-}
+greet(user);
 
-const value:number=sum(2,3);
-console.log(value);
-
-greet("world")
-
-// creating function to check your age is legal or not .
-function isLegal(age:number):Boolean{
-    if(age>=18){return true;}
-    else{
-        return false;
-    }
-
-}
-
-console.log(isLegal(20));
-
-// passing a function as a argument so that a function runs after 1 sec .
-function after1sec(fnc:()=>void){
-    setTimeout(fnc,1000);
-}
-after1sec(function(){
-    console.log("hello after 1 sec")
-})
-
-const firstname=()=>{console.log("helllo world")};
+    
 
 
-const greeting=(name:string)=>{ `hello ${name}`};
