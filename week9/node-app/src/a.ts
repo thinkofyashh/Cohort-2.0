b805e3bd2f1dfe7e46c26b32b659580878319639
yashrawat <yashrawat682@gmail.com>
2025-01-14 18:36:16 +0530

    corrected Arrays in TS

--- week9/node-app/src/a.ts
+++ week9/node-app/src/a.ts
@@ -1,16 +1,33 @@
-type Person={
-    firstname:string,
-    lastname: string,
-    age: number,
+
+type arrType=number[];
+
+function Maxvalue(arr:arrType):number{
+    let maxi=arr[0];
+
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]>maxi) maxi=arr[i];
+    }
+    return maxi;
+
 }
 
+console.log(Maxvalue([1,2,344,5,5]));
 
-function greet(id:(string | number)):void{
-    console.log(`Hello, ${id}`);
+interface user{
+    firstname:string ,
+    lastname:string,
+    age:number
 }
 
-greet(12);
-greet("12");
-    
-
+function filterout(users:user[]):user[]{
+   
+    return users.filter(x=>x.age>=18)
+        
+}
+console.log(filterout([{
+    firstname:"yash",lastname:"rawat",age:12
+},{
+    firstname:"yash1",lastname:"rawat1",age:20
+}])
+);
 
