f61ec8d4309380589be76fb8e7e2322a9978b375
yashrawat <yashrawat682@gmail.com>
2025-01-14 18:55:22 +0530

    enum in typescript

--- week9/node-app/src/a.ts
+++ week9/node-app/src/a.ts
@@ -1,33 +1,18 @@
+enum Direction{
+    up="up",   
+    down='down' , 
+    left="left" ,  
+    right="right" 
+}
+console.log(Direction.up);
 
-type arrType=number[];
-
-function Maxvalue(arr:arrType):number{
-    let maxi=arr[0];
-
-    for(let i=0;i<arr.length;i++){
-        if(arr[i]>maxi) maxi=arr[i];
-    }
-    return maxi;
+function pressed(keyPressed:Direction):void{
+    if(keyPressed==Direction.up) console.log("up going");
+    else if(keyPressed==Direction.down) console.log("down going");
 
 }
 
-console.log(Maxvalue([1,2,344,5,5]));
+pressed(Direction.up);
+pressed(Direction.down);
 
-interface user{
-    firstname:string ,
-    lastname:string,
-    age:number
-}
-
-function filterout(users:user[]):user[]{
-   
-    return users.filter(x=>x.age>=18)
-        
-}
-console.log(filterout([{
-    firstname:"yash",lastname:"rawat",age:12
-},{
-    firstname:"yash1",lastname:"rawat1",age:20
-}])
-);
 
