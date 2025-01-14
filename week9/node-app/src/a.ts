1b11bd16b78ff8cac63b30642a4fd1d3fb71565a
yashrawat <yashrawat682@gmail.com>
2025-01-14 19:48:42 +0530

    with interface and using generics together

--- week9/node-app/src/a.ts
+++ week9/node-app/src/a.ts
@@ -2,8 +2,14 @@ function identity<t>(arg:t[]):t{
     return arg[0];
 }
 
+interface User{
+    firstname: string;
+}
 
 const iden1=identity<string>(["hello","right","lefy"]);
 const iden2=identity<number>([1,23,43,22,4,4]);
+const iden3=identity<User>([{firstname:"yash"}])
+
+console.log(iden3)
 
 console.log(iden1.toUpperCase());
