6062a896089981fa80d308a0a5affa04e0868a7e
yashrawat <yashrawat682@gmail.com>
2025-01-23 17:58:23 +0530

    impelementation of Middlewares using hono framework

--- week11/hono-app/src/index.ts
+++ week11/hono-app/src/index.ts
@@ -2,6 +2,16 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
+async function authMiddleWare(c:any,next:any){
+if(c.req.header.Authorization){
+  // middleware checks or logic 
+  await next();
+}else{
+  return c.text("you couldnt access the services sorry")
+}
+}
+
+app.use(authMiddleWare);
  app.post('/',async (c) => {
   const body =await  c.req.json();
   console.log(body);
