8d1e8c75f937336c372debbe6d2bc2f2f2a1fa86
yashrawat <yashrawat682@gmail.com>
2025-04-26 16:38:40 +0530

    understood about the components in next js

--- week14/next-app/app/(auth)/layout.tsx
+++ week14/next-app/app/(auth)/layout.tsx
@@ -4,7 +4,6 @@ export default function({children}:{children:React.ReactNode}){
         <div style={{ padding: '2rem', backgroundColor: '#f9f9f9' }}>
       <h2>Welcome to the Signup Section 🚀</h2>
       <div>{children}</div>
-      <div>hfakfbkbfk</div>
     </div>
         </>
     )
