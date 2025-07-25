9c809a891cbc47dac9abdae208644fbb5f2f1b72
yashrawat <yashrawat682@gmail.com>
2025-07-25 16:43:27 +0530

    learned about  the ssg(server site genereation)

---
+++ week19/week19-ssg/app/page.tsx
@@ -0,0 +1,15 @@
+
+
+export default async function Home() {
+  {console.log("hello world "),{
+    next: { revalidate: 10 }
+}}
+   
+  return (
+    <div >
+      not hello 
+
+    </div>
+    
+  );
+}
